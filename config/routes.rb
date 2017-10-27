be9e1b94485439dc2bfa38769b6c1cfe75a65f4c
William Van Deren <wvanderen@gmail.com>
2017-10-27 13:24:16 +0000

    checkpoint2-first

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   get 'welcome/index'
 
   get 'welcome/about'

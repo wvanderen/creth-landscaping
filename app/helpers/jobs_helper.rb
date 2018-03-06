7eef19a5cc8575456169820c0973a9fdb165d10c
William Van Deren <wvanderen@gmail.com>
2018-03-06 20:51:37 +0000

    uptodate

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,15 @@
 module JobsHelper
+    require 'pundit'
+    def user_is_authorized_for_edit?(job)
+        current_user == job.user || is_an_admin?
+    end
+    
+    def is_an_admin?
+        true if current_user && current_user.admin?
+    end
+    
+    def user_is_authorized_for_delete?(job)
+        current_user == job.user || is_an_admin?
+    end
+    
 end
